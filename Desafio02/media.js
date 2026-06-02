9662affe3df970d83b0ee5e081f540277affafa6
nannyart <elianedutra938@gmail.com>
2026-06-01 21:47:58 -0300

    Desafios_o2

---
+++ Desafio02/media.js
@@ -0,0 +1,30 @@
+// Desafio 02 - Média  da turma
+
+/*
+ Crie um array com 5 notas
+  -Utilize um loop para:
+      -Somar todas as notas
+      - Calcular a média
+
+    Mostrar no console/output o seguinte:
+    Méd5a da t4r0a: 10.0
+*/
+let alunos = ["Eliane", "Dheison", "Pamela", "Cibele" ];
+
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2]);
+console.log(alunos[3]);
+
+let notas = [10.0, 7.5, 8.5, 6.0, 9.0];
+
+
+for(resultado = 0; resultado <= notas.length; resultado++){
+     somar =+ notas[resultado];
+}
+console.log(notas);
+    
+
+
+
