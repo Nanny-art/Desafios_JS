9388420301717ad85cdd689c982a19522c816ea9
theNannyart <elianedutra938@gmail.com>
2026-06-16 19:08:11 -0300

    atualização

---
+++ Desafio02/boletim.js
@@ -0,0 +1,25 @@
+/*
+DESAFIO 03 - SISTEMA DE APROVAÇÃO
+
+Crie uma função chamada verificarAluno() que receba:
+-nome
+-nota
+
+Regras:
+- Notas maior ou igual a 7  -> Aprovado
+- Nota maior ou igual a 5 e menor que 7 -> Recuperação
+- Nota menor que 5 -> Reprovado
+
+Exiba uma mensagem informando a situação do aluno.
+*/
+
+function verificadorAluno(nome, nota) {
+    if(nota >= 7){
+        console.log(nome + "está aprovado");
+    }
+    else if(nota >= 5){
+        console.log
+    }
+}
+
+
